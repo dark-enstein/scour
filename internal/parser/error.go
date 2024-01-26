65d21452f7ce4af544ed56089bed5963ae60079e
Ayobami Bamigboye <>
2024-01-26 19:46:34 +0000

    chore(SCO-4): add more docs

--- internal/parser/error.go
+++ internal/parser/error.go
@@ -1,8 +1,12 @@
 package parser
 
+// IsIPv6Unsupported checks if the given error is specifically an IPv6 unsupported error.
+// It returns true if the error is exactly ErrIPv6tError, indicating an issue with IPv6 support.
+// Otherwise, it returns false, suggesting that the error is of a different type or nil.
 func IsIPv6Unsupported(e error) bool {
+	// Check if the provided error is the specific IPv6 unsupported error (ErrIPv6tError).
 	if e != ErrIPv6tError {
-		return false
+		return false // Return false if the error is different.
 	}
-	return true
+	return true // Return true if the error matches ErrIPv6tError.
 }
